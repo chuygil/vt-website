3a0d11d2c0661de6ede61c1e30a44a9511827014
Jesus Gil <jesusgil.dev@gmail.com>
2021-03-31 15:43:09 -0500

    hero image
    Changed hero image to use nextjs Image component

--- components/hero-section.js
+++ components/hero-section.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import styled from 'styled-components';
 
 const SectionWrapper = styled.section`
@@ -14,17 +15,9 @@ const SectionWrapper = styled.section`
 const LogoImageWrapper = styled.div`
   justify-self: center;
   align-self: center;
-  position: relative;
   width: 300px;
   height: 300px;
-  overflow: hidden;
   border-radius: var(--radius-sm);
-
-  > img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
 `;
 
 const HeroInfoWrapper = styled.div`
@@ -37,7 +30,13 @@ function HeroSection() {
   return (
     <SectionWrapper>
       <LogoImageWrapper>
-        <img src="/images/logo.jpeg" alt="No Era Penal Logo" />
+        <Image
+          src="/images/logo.jpeg"
+          alt="No Era Penal Logo"
+          width={300}
+          height={300}
+          priority
+        />
       </LogoImageWrapper>
       <HeroInfoWrapper>
         <h1>No Era Penal</h1>
